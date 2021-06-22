4fc1b384a456ba176e495be8ef82cb0e399d93d1
Katzen48 | Tobi <Katzen48@freenet.de>
2021-06-22 18:06:48 +0200

    replaced new raw event type strings with laravel-twitch constants

--- src/Events/EventParser.php
+++ src/Events/EventParser.php
@@ -40,11 +40,11 @@ class EventParser
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelUpdateEvent::class,
             EventSubType::CHANNEL_SUBSCRIBE =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelSubscribeEvent::class,
-            EventSubType::CHANNEL_UNSUBSCRIBE => // TODO change to EventSubType::CHANNEL_SUBSCRIPTION_END
+            EventSubType::CHANNEL_SUBSCRIPTION_END =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelUnsubscribeEvent::class,
-            'channel.subscription.gift' => // TODO change to EventSubType::CHANNEL_SUBSCRIPTION_GIFT
+            EventSubType::CHANNEL_SUBSCRIPTION_GIFT =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelSubscriptionGiftEvent::class,
-            'channel.subscription.message' => // TODO change to EventSubType::CHANNEL_SUBSCRIPTION_MESSAGE
+            EventSubType::CHANNEL_SUBSCRIPTION_MESSAGE =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelSubscriptionMessageEvent::class,
             EventSubType::CHANNEL_CHEER =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelCheerEvent::class,
@@ -68,19 +68,19 @@ class EventParser
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelPointsCustomRewardRedemptionAddEvent::class,
             EventSubType::CHANNEL_CHANNEL_POINTS_CUSTOM_REWARD_REDEMPTION_UPDATE =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelPointsCustomRewardRedemptionUpdateEvent::class,
-            'channel.poll.begin' =>  // TODO change to EventSubType::CHANNEL_POLL_BEGIN
+            EventSubType::CHANNEL_POLL_BEGIN =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelPollBeginEvent::class,
-            'channel.poll.progress' =>  // TODO change to EventSubType::CHANNEL_POLL_PROGRESS
+            EventSubType::CHANNEL_POLL_PROGRESS =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelPollProgressEvent::class,
-            'channel.poll.ended' =>  // TODO change to EventSubType::CHANNEL_POLL_END
+            EventSubType::CHANNEL_POLL_END =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelPollEndEvent::class,
-            'channel.prediction.begin' =>  // TODO change to EventSubType::CHANNEL_PREDICTION_BEGIN
+            EventSubType::CHANNEL_PREDICTION_BEGIN =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelPredictionBeginEvent::class,
-            'channel.prediction.progress' =>  // TODO change to EventSubType::CHANNEL_PREDICTION_PROGRESS
+            EventSubType::CHANNEL_PREDICTION_PROGRESS =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelPredictionProgressEvent::class,
-            'channel.prediction.lock' =>  // TODO change to EventSubType::CHANNEL_PREDICTION_LOCK
+            EventSubType::CHANNEL_PREDICTION_LOCK =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelPredictionLockEvent::class,
-            'channel.prediction.ended' =>  // TODO change to EventSubType::CHANNEL_PREDICTION_END
+            EventSubType::CHANNEL_PREDICTION_END =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelPredictionEndEvent::class,
             EventSubType::CHANNEL_HYPE_TRAIN_BEGIN =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelHypeTrainBeginEvent::class,
@@ -89,7 +89,7 @@ class EventParser
             EventSubType::CHANNEL_HYPE_TRAIN_END =>
                 \katzen48\Twitch\EventSub\Events\Channel\ChannelHypeTrainEndEvent::class,
 
-            'extension.bits_transaction.create' => // TODO change to EventSubType::EXTENSION_BITS_TRANSACTION_CREATE
+            EventSubType::EXTENSION_BITS_TRANSACTION_CREATE =>
                 \katzen48\Twitch\EventSub\Events\Extension\ExtensionBitsTransactionCreateEvent::class,
 
             EventSubType::USER_AUTHORIZATION_REVOKE =>
